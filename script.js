e9cc5e182c0c3b889c09169534e896ecdbd624c0
Afzal-Mia <mohammedafzal1213@gmail.com>
2024-07-27 00:25:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputText = document.getElementById('inputText');
+const generateBtn = document.getElementById('generateBtn');
+const qrCode = document.getElementById('qrCode');
+const downloadBtn = document.getElementById('downloadBtn');
+
+//even the husband for the query of the department onto a pri
+generateBtn.addEventListener('click', () => {
+  const text = inputText.value.trim();
+  qrCode.innerHTML='';
+  if (text === '') return;
+  new QRCode(qrCode, {
+    text: text,
+    width:400 ,
+    height: 400,
+  });
+
+  downloadBtn.style.display = 'block';
+downloadBtn.addEventListener('click', ()=>{
+    let img = document.querySelector('#qrCode img');
+    if(img !== null){
+      let imgAtrr = img.getAttribute('src');
+      downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+        // if nothing can be found then it generates a blank rectangle canvas
+    }
+});
+});
